f14b0553e381c033d16f2a4f139b8251c5b477fd
Aman07a <aman16.aa17@gmail.com>
2024-03-27 09:57:30 +0100

    Tasks completed

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # Defines the root path route ("/")
+  root 'pages#home'
+
+  # Add a new route for the about page ("/about")
+  get "about" => "pages#about"
 end
